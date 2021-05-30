85763f9069455eace17b286261b6835611ed433b
unknown <chithrakarthik321@gmail.com>
2021-05-31 00:44:16 +0530

    static file update

--- quiz_project/settings.py
+++ quiz_project/settings.py
@@ -126,8 +126,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR/ 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR/ 'quizes'/'static'),)
+
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static/quizes')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 django_heroku.settings(locals())
 
 
